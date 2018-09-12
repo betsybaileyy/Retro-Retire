2d9f6608f3b3096b23173e89cbec96e90ea4dbe0
betsybaileyy <betsy.bailey80@gmail.com>
2018-09-11 18:32:28 -0700

    users can see all reviews

--- app.js
+++ app.js
@@ -2,15 +2,24 @@ const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars');
 
+let reviews = [
+  { title: "Great Review" },
+  { title: "Next Review" }
+]
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => {
-  res.render('home', { msg: 'Hello World!' });
-})
+// app.get('/', (req, res) => {
+//   res.render('home', { msg: 'Hello World!' });
+// })
+//
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.render('reviews-index', { reviews: reviews});
 })
 
 app.listen(3000, () => {
