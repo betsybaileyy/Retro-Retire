248b98a7f5af7dd44d25059295e4737a52f0b578
betsybaileyy <betsy.bailey80@gmail.com>
2018-09-19 12:11:20 -0700

    Added MongoDB and Review Model

--- app.js
+++ app.js
@@ -2,21 +2,32 @@ const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars');
 
-let reviews = [
-  { title: "Great Review" },
-  { title: "Next Review" }
-]
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true});
+
+const Review = mongoose.model('Review', {
+    title: String
+});
+
+
+
+// let reviews = [
+//   { title: "Great Review!" },
+//   { title: "Next Review" }
+// ]
+
+app.get('/', (req, res) => {
+    Review.find()
+        .then(reviews => {
+            res.render('reviews-index', { reviews: reviews });
+        })
+        .catch(err => {
+            console.log(err);
+        })
+})
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-
-// app.get('/', (req, res) => {
-//   res.render('home', { msg: 'Hello World!' });
-// })
-//
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-// })
 
 app.get('/', (req, res) => {
     res.render('reviews-index', { reviews: reviews});
@@ -25,3 +36,19 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
 })
+
+
+// Model.deleteMany()
+// Model.deleteOne()
+// Model.find()
+// Model.findById()
+// Model.findByIdAndDelete()
+// Model.findByIdAndRemove()
+// Model.findByIdAndUpdate()
+// Model.findOne()
+// Model.findOneAndDelete()
+// Model.findOneAndRemove()
+// Model.findOneAndUpdate()
+// Model.replaceOne()
+// Model.updateMany()
+// Model.updateOne()
