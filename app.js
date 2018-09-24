a3ce939a3848cfed03b4cebff53e29da5e34a897
betsybaileyy <betsy.bailey80@gmail.com>
2018-09-24 10:18:35 -0700

    Users can create reviews

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const app = express()
 var exphbs = require('express-handlebars');
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true});
+mongoose.connect('mongodb://localhost/rotten-potatoes', {
+    useMongoClient: true
+});
 
 const Review = mongoose.model('Review', {
-    title: String
+    title: String,
+    description: String,
+    movieTitle: String
 });
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 
 
@@ -19,22 +28,46 @@ const Review = mongoose.model('Review', {
 app.get('/', (req, res) => {
     Review.find()
         .then(reviews => {
-            res.render('reviews-index', { reviews: reviews });
+            res.render('reviews-index', {
+                reviews: reviews
+            });
         })
         .catch(err => {
             console.log(err);
         })
 })
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-    res.render('reviews-index', { reviews: reviews});
+    res.render('reviews-index', {
+        reviews: reviews
+    });
+})
+
+app.get('/reviews/new', (req, res) => {
+    res.render('reviews-new', {});
+})
+
+app.post('/reviews', (req, res) => {
+    console.log(req.body);
+    res.render('reviews-new', {});
+})
+
+app.post('/reviews', (req, res) => {
+    Review.create(req.body).then((review) => {
+        console.log(review);
+        res.redirect('/');
+    }).catch((err) =>{
+        console.log(err.message);
+    })
 })
 
 app.listen(3000, () => {
-  console.log('App listening on port 3000!')
+    console.log('App listening on port 3000!')
 })
 
 
