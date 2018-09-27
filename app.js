70ea401f4aace891e5d5a029eba068054d750746
betsybaileyy <betsy.bailey80@gmail.com>
2018-09-27 16:18:58 -0700

    heroku setup

--- app.js
+++ app.js
@@ -2,16 +2,10 @@ const express = require('express')
 const methodOverride = require('method-override')
 const app = express()
 var exphbs = require('express-handlebars');
-
+const reviews = require('./controllers/reviews');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/rotten-potatoes', {
     useMongoClient: true
-});
-
-const Review = mongoose.model('Review', {
-    title: String,
-    description: String,
-    movieTitle: String
 });
 
 const bodyParser = require('body-parser');
@@ -20,73 +14,18 @@ app.use(methodOverride('_method'))
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    Review.find()
-        .then(reviews => {
-            res.render('reviews-index', {
-                reviews: reviews
-            });
-        })
-        .catch(err => {
-            console.log(err);
-        })
-})
-
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
 
-app.get('/reviews/new', (req, res) => {
-    res.render('reviews-new', {});
-})
-
-app.get('/reviews/:id', (req, res) => {
-    Review.findById(req.params.id).then((review) => {
-        res.render('reviews-show', { review: review })
-    }).catch((err) => {
-        console.log(err.message);
-    })
-});
-
-app.post('/reviews', (req, res) => {
-    Review.create(req.body).then((review) => {
-        console.log(review);
-        res.redirect(`/reviews/${review._id}`);
-    }).catch((err) =>{
-        console.log(err.message);
-    })
-})
-
-app.get('/reviews/:id/edit', (req, res) => {
-    Review.findById(req.params.id, function(err, review) {
-        res.render('reviews-edit', {review: review});
-    })
-})
-
-app.put('/reviews/:id', (req, res) => {
-  Review.findByIdAndUpdate(req.params.id, req.body)
-    .then(review => {
-      res.redirect(`/reviews/${review._id}`)
-    })
-    .catch(err => {
-      console.log(err.message)
-    })
-})
-
-app.delete('/reviews/:id', function (req, res) {
-  console.log("DELETE review")
-  Review.findByIdAndRemove(req.params.id).then((review) => {
-    res.redirect('/');
-  }).catch((err) => {
-    console.log(err.message);
-  })
-})
+require('./controllers/reviews')(app);
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
 })
 
+module.exports = app;
 
 // Model.deleteMany()
 // Model.deleteOne()
